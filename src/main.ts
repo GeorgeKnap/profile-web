7f131114c044b36c812b2d3043fefca943767376
Jiří Knap <jirka.knap@gmail.com>
2018-03-05 22:29:27 +0100

    cannt find name 'module' fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { hmrBootstrap } from './hmr';
 
+declare var module: NodeModule;
+
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
 if (environment.production) {
