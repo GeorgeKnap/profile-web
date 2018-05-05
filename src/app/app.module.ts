1f29160509701eea0725210121dfa323a5d94df3
Jiří Knap <jirka.knap@gmail.com>
2018-05-05 20:52:59 +0200

    CV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ParallaxModule } from 'ngx-parallax';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-
-import { MaterialModule } from './shared/material.module';
-import { CoreModule } from './core.module';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { ParallaxModule } from 'ngx-parallax';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core.module';
 import { LayoutModule } from './layout/scripts/layout.module';
+import { MaterialModule } from './shared/material.module';
 import { WildcardRoutingModule } from './wildcard-routing.module';
 
-import { environment } from '../environments/environment';
+
 
 // AoT requires an exported function for factories
 export function httpLoaderFactory(http: HttpClient) {
@@ -40,6 +41,7 @@ export function httpLoaderFactory(http: HttpClient) {
         ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
         AngularFireModule.initializeApp(environment.firebase, 'george-knap'),
         AngularFireDatabaseModule,
+        AngularFireStorageModule,
         MaterialModule,
         AppRoutingModule,
         CoreModule,
--- src/app/dashboard/cards/about-me/about-me.component.ts
+++ src/app/dashboard/cards/about-me/about-me.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'gk-about-me',
@@ -7,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
-  myAge: number = 33;
+  myAge = 33;
+  cvUrl$: Observable<string>;
+
+  constructor(
+    private readonly afStorage: AngularFireStorage
+  ) {
+    this.cvUrl$ = this.afStorage.ref('CV.pdf').getDownloadURL();
+  }
+
+  downloadCv() {
+    this.cvUrl$.pipe(first()).subscribe(url => window.open(url, '_blank'));
+  }
 
   toAngular() {
     window.open('https://angular.io/', '_blank');
@@ -29,8 +43,8 @@ export class AboutMeComponent implements OnInit {
     window.open('https://nativescript.org/', '_blank');
   }
 
+
   ngOnInit() {
-    //
   }
 
 }
